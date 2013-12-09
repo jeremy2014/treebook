5952a101daf2837bedd8d7e57bfe672ff36f6f1f
root <root@jeremy-Aspire-4333.(none)>
2013-12-09 12:52:01 +0800

    updated the treebook

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,16 @@
+class StudentsController < ApplicationController
+
+def new
+	@student = Student.new
+end
+
+def create
+	@student = Student.new(params[:student])
+	if @student.save
+	redirect_to new_student_path
+	end
+end
+
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131129062649) do
+ActiveRecord::Schema.define(version: 20131129085141) do
 
   create_table "statuses", force: true do |t|
     t.string   "name"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "firstname"
+    t.string   "lastname"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
